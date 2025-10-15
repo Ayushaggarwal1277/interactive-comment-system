5cc80c4bff82baaa8075a020426bf8398572bd83
Ayush Aggarwal <140179769+Ayushaggarwal1277@users.noreply.github.com>
2025-10-16 00:29:56 +0530

    Add new CORS origin for production environment

--- Backend/app.js
+++ Backend/app.js
@@ -15,6 +15,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:5175',
+  'https://interactive-comment-system.vercel.app/',
   process.env.CORS_ORIGIN
 ].filter(Boolean);
 
