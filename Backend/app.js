eb0f53053dcfbf30b7711dd5ed763ca58d80e26c
Ayush Aggarwal <140179769+Ayushaggarwal1277@users.noreply.github.com>
2025-10-16 01:01:04 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:5175',
-  'http://interactive-comment-system.vercel.app/',
+
   process.env.CORS_ORIGIN
 ].filter(Boolean);
 
